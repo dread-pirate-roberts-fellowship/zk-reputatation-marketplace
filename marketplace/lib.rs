b62ad1911da429d5b11d973465f2a46facbb786b
lean-apple <lea@parity.io>
2023-04-01 23:58:39 +0100

    fix events

--- marketplace/lib.rs
+++ marketplace/lib.rs
@@ -6,24 +6,23 @@ mod marketplace {
     use risc0_zkvm::sha::Digest;
 
     use ink::{
-        prelude::{collections::BTreeSet, string::String, vec::Vec, string::ToString},
+        prelude::{collections::BTreeSet, string::String, string::ToString, vec::Vec},
         storage::Mapping,
         LangError,
     };
 
     #[ink(storage)]
-    
+
     pub struct Marketplace {
         /// List of all users.
         users: Vec<UserProfile>,
         /// List of all assets.
         assets: Vec<Asset>,
         current_sale: Sale,
-        /// Mapping between Hash and bool 
-        spent_nullifier: Mapping<Hash, bool>, 
-        commitments: BTreeSet<Hash>
+        /// Mapping between Hash and bool
+        spent_nullifier: Mapping<Hash, bool>,
+        commitments: BTreeSet<Hash>,
     }
-
 
     #[derive(scale::Decode, scale::Encode)]
     #[cfg_attr(
@@ -31,10 +30,10 @@ mod marketplace {
         derive(Debug, scale_info::TypeInfo, ink::storage::traits::StorageLayout)
     )]
     pub struct Sale {
-        status: String, //Write like an enum after 
+        status: String, //Write like an enum after
         prize: u32,
         asset_id: u32,
-        // seller_reputation - pending reputation to add in a way 
+        // seller_reputation - pending reputation to add in a way
     }
 
     #[derive(scale::Decode, scale::Encode)]
@@ -43,7 +42,7 @@ mod marketplace {
         derive(Debug, scale_info::TypeInfo, ink::storage::traits::StorageLayout)
     )]
     pub struct UserProfile {
-        account_id: AccountId, 
+        account_id: AccountId,
         reputation: u32,
     }
 
@@ -60,62 +59,63 @@ mod marketplace {
         purchasable: bool,
     }
 
+    /// Event emitted when an iten is put on sale
+    #[ink(event)]
+    pub struct ItemOnsale {
+        seller_id: AccountId,
+    }
+
+    /// Event emitted when an iten is bought
+    #[ink(event)]
+    pub struct ItemBought {
+        seller_id: AccountId,
+    }
+
     impl Marketplace {
         /// Constructor that initializes the marketplace
         #[ink(constructor)]
-        pub fn new(assets_list: Vec<Asset>, users_list: Vec<UserProfile>, ) -> Self {
+        pub fn new(assets_list: Vec<Asset>, users_list: Vec<UserProfile>) -> Self {
             let init_sale = Sale {
                 status: "Closed".to_string(),
                 prize: 0,
-                asset_id : 10
+                asset_id: 10,
             };
             let mk = Self {
                 users: Vec::new(),
                 assets: assets_list,
-                current_sale: init_sale, 
+                current_sale: init_sale,
                 //TODO: initialize spent_nullifier
-                commitments: BTreeSet::new()
+                commitments: BTreeSet::new(),
             };
             mk
         }
-        
-        /// Event emitted when an iten is put on sale
-        #[ink(event)]
-        pub struct ItemOnsale {
-            seller_id: AccountId,
-        }
-
-        /// Event emitted when an iten is bought
-        #[ink(event)]
-        pub struct ItemBought
-         {
-            seller_id: AccountId,
-        }
- 
 
         #[ink(message)]
-        ///Register new seller 
-        pub fn register_seller(&self, new_hash: Hash)  {
+        ///Register new seller
+        pub fn register_seller(&self, new_hash: Hash) {
             self.commitments.insert(new_hash);
         }
 
-        /// Modify Item on Sale 
-        pub fn put_asset_on_sale(mut self, mut asset: Asset, zk_proof: Digest, account: AccountId) -> Result<u32, LangError>  {
+        /// Modify Item on Sale
+        pub fn put_asset_on_sale(
+            mut self,
+            mut asset: Asset,
+            zk_proof: Digest,
+            account: AccountId,
+        ) -> Result<u32, LangError> {
             if !asset.purchasable {
                 asset.purchasable = true;
                 let ongoing_sale = Sale {
-                   status: "OnGoing".to_string(), 
-                   prize: 0, 
-                   asset_id: asset.id
+                    status: "OnGoing".to_string(),
+                    prize: 0,
+                    asset_id: asset.id,
                 };
                 self.current_sale = ongoing_sale;
                 // Verify the proof of reputation
                 // Put nft in the contract, and set the price
                 // ybort abort if nullifier was spent
             }
-            self.env().emit_event(ItemOnsale {
-                seller_id: account
-            }); 
+            self.env().emit_event(ItemOnsale { seller_id: account });
             // TODO: Add Result output
         }
 
@@ -123,19 +123,18 @@ mod marketplace {
         pub fn buy_asset(&mut self, asset: Asset, account: AccountId, price: u32) {
             // check balance of account, compare to price
             // transfer of the account Id to the asset
-            self.env().emit_event(ItemBought {
-                seller_id: account
-            }); 
+            self.env().emit_event(ItemBought { seller_id: account });
         }
 
         #[ink(message)]
         pub fn give_seller_review(&mut self, seller: AccountId, encrypted_change: Vec<u8>) {
-            //Check 
-            //emit Event 
+            //TODO: Check sellerId
+            //Update seller review
+            self.env().emit_event(ItemBought { seller_id: seller });
         }
 
         #[ink(message)]
-        pub fn update_seller_reputation(&self, hash: Hash, review_proof:Digest) {
+        pub fn update_seller_reputation(&self, hash: Hash, review_proof: Digest) {
             //TBD
         }
     }
